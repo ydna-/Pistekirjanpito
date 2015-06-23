c6cf23e13daf88a2de3d3a5552cb709818519285
Andreas Niskanen <andreas.niskanen@helsinki.fi>
2015-06-23 20:49:53 +0300

    more jquery!

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,3 +1,64 @@
+function toggle(elem) {
+    if (elem.hasClass("star-checkbox")) {
+        switch (elem.val()) {
+            case "":
+                elem.val("V");
+                elem.prop('indeterminate', true);
+                break;
+            case "V":
+                elem.val("O");
+                elem.prop('indeterminate', false);
+                elem.prop('checked', true);
+                break;
+            case "O":
+                elem.val("");
+                elem.prop('indeterminate', false);
+                elem.prop('checked', false);
+                break;
+            default:
+                return;
+        }
+    } else if (elem.hasClass("ns-checkbox")) {
+        switch (elem.val()) {
+            case "":
+                elem.val("O");
+                elem.prop('checked', true);
+                break;
+            case "O":
+                elem.val("");
+                elem.prop('checked', false);
+                break;
+            default:
+                return;
+        }
+    }
+}
+
+function selectAll() {
+    $(".ns-checkbox").each(function () {
+        $(this).val("O");
+        $(this).prop('checked', true);
+    });
+    $(".star-checkbox").each(function () {
+        $(this).val("O");
+        $(this).prop('indeterminate', false);
+        $(this).prop('checked', true);
+    });
+    
+}
+
+function removeAll() {
+    $(".ns-checkbox").each(function () {
+        $(this).val("");
+        $(this).prop('checked', false);
+    });
+    $(".star-checkbox").each(function () {
+        $(this).val("");
+        $(this).prop('indeterminate', false);
+        $(this).prop('checked', false);
+    });
+}
+
 $(document).ready(function () {
     $('form.destroy-form').on('submit', function (submit) {
         var confirm_message = $(this).attr('data-confirm');
@@ -67,4 +128,71 @@ $(document).ready(function () {
             }
         });
     });
+});
+
+$(document).keydown(function (e) {
+    switch (e.which) {
+        case 49:
+            toggle($("#p1"));
+            break;
+        case 50:
+            toggle($("#p2"));
+            break;
+        case 51:
+            toggle($("#p3"));
+            break;
+        case 52:
+            toggle($("#p4"));
+            break;
+        case 53:
+            toggle($("#p5"));
+            break;
+        case 54:
+            toggle($("#p6"));
+            break;
+        case 55:
+            toggle($("#p7"));
+            break;
+        case 56:
+            toggle($("#p8"));
+            break;
+        case 57:
+            toggle($("#p9"));
+            break;
+        case 48:
+            toggle($("#p10"));
+            break;
+        case 81:
+            toggle($("#p11"));
+            break;
+        case 87:
+            toggle($("#p12"));
+            break;
+        case 69:
+            toggle($("#p13"));
+            break;
+        case 82:
+            toggle($("#p14"));
+            break;
+        case 84:
+            toggle($("#p15"));
+            break;
+        case 89:
+            toggle($("#p16"));
+            break;
+        case 85:
+            toggle($("#p17"));
+            break;
+        case 73:
+            toggle($("#p18"));
+            break;
+        case 79:
+            toggle($("#p19"));
+            break;
+        case 80:
+            toggle($("#p20"));
+            break;
+        default:
+            return;
+    }
 });
