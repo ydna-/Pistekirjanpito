42c0171ff9b4cd96ba5274e2d8285b9d005a3759
root <root@pikipi.mathstat.helsinki.fi>
2015-09-04 16:35:38 +0300

    puolipiste

--- config/environment.sh
+++ config/environment.sh
@@ -3,5 +3,5 @@
 # korvaa tämä omalla käyttäjätunnuksellasi
 USERNAME="andreasn"
 # korvaa tämä haluamallasi kansion nimellä
-PROJECT_FOLDER="pkp"
+PROJECT_FOLDER=""
 # sovelluksesi tulee sijaitsemaan osoitteessa USERNAME.users.cs.helsinki.fi/PROJECT_FOLDER
